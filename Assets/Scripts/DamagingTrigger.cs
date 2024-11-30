47a857aed2551ac8faf4cb982d2222ff1be0d944
KevinYang <yangzijin@gmail.com>
2024-11-29 21:08:28 -0800

    player sprite, damaging trigger added

--- Assets/Scripts/DamagingTrigger.cs
+++ Assets/Scripts/DamagingTrigger.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collectible : MonoBehaviour
+public class DamagingTrigger : MonoBehaviour
 {
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+   player = GameObject.FindWithTag("Player");     
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(GameObject.ReferenceEquals(other.gameObject, player)){
+            Debug.Log("player2");
+            player.GetComponent<PlayerScript>().TakeDamage();
+        }
     }
 
-<<<<<<< Updated upstream:Assets/Scripts/collectible.cs
-=======
-    private void OnTriggerEnter2D(Collider2D other) {
-        player.GetComponent<PlayerScript>().waxNum++;
-        Destroy(gameObject);
-    }
->>>>>>> Stashed changes:Assets/Scripts/waxShard.cs
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using Unity.VisualScripting;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] float jumpSpeed;
+    Vector2 moveDir = Vector2.zero;
+    Rigidbody2D rb;
+    BoxCollider2D cldr;
+
+    //wax counter
+    public int waxNum;
+
+    //double jump
+    // float jumpTimer = 0;
+    bool canDoubleJump = false;
+    int groundMask;
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -Vector2.up, cldr.bounds.extents.y + 0.1f, groundMask);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cldr = GetComponent<BoxCollider2D>();
+        rb.freezeRotation = true;
+        groundMask = LayerMask.GetMask("Ground");
+        waxNum = 0;
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 inputDir = value.Get<Vector2>();
+        moveDir = inputDir;
+    }
+
+    void OnJump(InputValue value){
+        if(isGrounded()){
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+        if(!isGrounded() && canDoubleJump){
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            canDoubleJump = false;
+        }
+    }
+    
+    public void TakeDamage(){
+        Debug.Log(waxNum);
+        if(waxNum >= 0){
+            waxNum--;
+        }
+        if(waxNum < 0){
+            die();
+        }
+        Debug.Log(waxNum);
+    }
+    
+    public void die(){
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded()){
+            canDoubleJump = true;
+        }
+        if(waxNum < 1){
+            canDoubleJump = false;
+        }
+        if(Mathf.Approximately(rb.velocity.y, 0) && waxNum > 1){
+            canDoubleJump = true;
+        }
+        rb.velocity = new Vector2(moveDir.x * movementSpeed, rb.velocity.y);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
