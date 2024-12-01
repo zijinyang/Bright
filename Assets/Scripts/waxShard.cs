03ad92e103968ebe6e024180b66c0851b3fba857
KevinYang <yangzijin@gmail.com>
2024-11-30 16:23:48 -0800

    script fixes

--- Assets/Scripts/waxShard.cs
+++ Assets/Scripts/waxShard.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class waxShard: MonoBehaviour
 {
-    [SerializeField] GameObject player;
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+       player = GameObject.FindWithTag("Player");     
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        player.GetComponent<playerMovement>().waxNum++;
+        player.GetComponent<PlayerScript>().waxNum++;
         Destroy(gameObject);
     }
     // Update is called once per frame
