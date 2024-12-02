3aa59f0c6196fa1c7a9c9bf36f532c6fd632be32
KevinYang <yangzijin@gmail.com>
2024-12-02 09:27:34 -0800

    Error fix

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Schema;
 using Unity.VisualScripting;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
